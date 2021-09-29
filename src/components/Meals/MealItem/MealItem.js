eb60267e840182b16a6672dd34f77e5fa566b770
Marcin <maciejewskim118@gmail.com>
2021-09-29 06:14:09 +0200

    Adding input + button element

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,21 @@
+import MealItemForm from "./MealItemForm";
+import classes from "./MealItem.module.css";
+
 const MealItem = (props) => {
-  return;
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li>
+      <div>
+        <h3 className={classes.meal}>{props.name}</h3>
+        <p className={classes.description}>{props.description}</p>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm />
+      </div>
+    </li>
+  );
 };
 
 export default MealItem;
